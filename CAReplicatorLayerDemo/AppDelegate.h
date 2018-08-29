fbbdcacdb5b4c6ab6f77dfbf8baf83e40898bed6
许锦伟 <xujinwei@xujinweideimac.local>
2018-08-29 13:44:27 +0800

    Initial Commit

---
+++ CAReplicatorLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAReplicatorLayerDemo
+//
+//  Created by 许锦伟 on 2018/8/29.
+//  Copyright © 2018年 许锦伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAReplicatorLayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CAReplicatorLayerDemo
+//
+//  Created by 许锦伟 on 2018/8/29.
+//  Copyright © 2018年 许锦伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
