54351490c982aff77caac8d03b69e59a78c92a6d
许锦伟 <xujinwei@xujinweideiMac.local>
2018-08-31 20:24:03 +0800

    项目提交

---
+++ CAReplicatorLayerDemo/CircularViewController.h
@@ -0,0 +1,13 @@
+//
+//  CircularViewController.h
+//  CAReplicatorLayerDemo
+//
+//  Created by 许锦伟 on 2018/8/31.
+//  Copyright © 2018年 许锦伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircularViewController : UIViewController
+
+@end
---
+++ CAReplicatorLayerDemo/RectViewController.h
@@ -0,0 +1,13 @@
+//
+//  RectViewController.h
+//  CAReplicatorLayerDemo
+//
+//  Created by 许锦伟 on 2018/8/31.
+//  Copyright © 2018年 许锦伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RectViewController : UIViewController
+
+@end
